b2c725f7924cb5852e8921601ad74c97c051b08c
Chase Lonas <chaselonas@gmail.com>
2024-03-12 20:14:49 -0400

    simple landing card

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class=" flex justify-center items-center h-full">
-    <h1 class=" text-6xl text-primary">Welcome to the homepage</h1>
+  <div>
+    <h1 class=" text-6xl text-primary grid place-content-center m-16">Welcome to the homepage</h1>
+    <div class=" mx-auto w-1/2 ">
+      <ULandingCard
+    title="Portfolio"
+    description="Veniam minim ipsum anim. Irure voluptate magna dolore id dolore ex quis sint sint et duis dolor enim fugiat."
+    icon="i-heroicons-photo"
+    color="primary"
+    orientation="horizontal"
+    >
+      <img
+        src="https://picsum.photos/640/360?grayscale"
+        class="w-full rounded-md"
+      />
+    </ULandingCard>
+    </div>
+    
   </div>
 </template>
